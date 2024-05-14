30d13d1e669b96bfb8bd5d6319589d9389ecd4d7
JeanOmeg <jeanomeg3@gmail.com>
2024-05-14 17:11:59 -0300

    atualização das query

--- src/utils/sql/create-table-template.sql
+++ src/utils/sql/create-table-template.sql
@@ -1,4 +1,4 @@
-CREATE TABLE tasks(  
+CREATE TABLE tasks(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
     status VARCHAR(255) NOT NULL,
@@ -6,7 +6,7 @@ CREATE TABLE tasks(
     updated VARCHAR(255) NOT NULL,
     id_user INT NOT NULL
 );
-CREATE TABLE users(  
+CREATE TABLE users(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL,
     real_name VARCHAR(255) NOT NULL,
